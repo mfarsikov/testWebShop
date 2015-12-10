8f6d9001c53c9dcb72dfa603fe3d0e50c9fab41f
Vagner88 <sbgalib@gmail.com>
2015-12-10 22:42:24 +0200

    Initial commit

---
+++ src/main/java/com/myshop/configuration/RootConfig.java
@@ -0,0 +1,10 @@
+package com.myshop.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Kot on 10.12.15.
+ */
+@Configuration
+public class RootConfig {
+}
