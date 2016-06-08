b617d1fe975d5090c041d4e4939ef16813a90aa2
Maksym Farsikov <maksym.farsikov@opower.com>
2016-06-08 23:03:50 +0300

    gradle changes for spring boot

---
+++ src/main/java/com/myshop/Main.java
@@ -0,0 +1,14 @@
+package com.myshop;
+
+/**
+ * Created by max on 6/8/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args)  {
+        SpringApplication.run(Main.class);
+    }
+}
